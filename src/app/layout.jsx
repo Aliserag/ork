d064727dd69389a17314652de92296e6a5da356e
Ali Serag <mr.ali.serag.eldin@gmail.com>
2024-07-23 20:32:27 -0700

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,7 +5,7 @@ import '@/styles/tailwind.css'
 export const metadata = {
   title: {
     template: '%s - Studio',
-    default: 'Studio - Award R&D firm based in Canada',
+    default: 'Studio - Award Winning R&D firm based in Canada',
   },
 }
 
